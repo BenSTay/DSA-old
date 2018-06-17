53290ce0537d1fd6ef5f82a304cf3097000b1cd8
Benjamin Taylor <staylor.ben@gmail.com>
2018-06-16 19:06:52 -0700

    Finished implementation of stacks and queues.

---
+++ Data Structures/Stack and Queue/Stack and Queue/Classes/Queue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack_and_Queue.Classes
+{
+    public class Queue
+    {
+        protected Node Front { get; set; }
+        protected Node Rear { get; set; }
+
+        /// <summary>
+        /// Creates a new instance of the Queue class.
+        /// </summary>
+        /// <param name="node">The first node in the queue.</param>
+        public Queue(Node node)
+        {
+            Front = node;
+            Rear = node;
+        }
+
+        /// <summary>
+        /// Adds a new node to the back of the Queue.
+        /// </summary>
+        /// <param name="node">The node being added.</param>
+        public void Enqueue(Node node)
+        {
+            Rear.Next = node;
+            Rear = node;
+        }
+
+        /// <summary>
+        /// Removes the node at the front of the Queue.
+        /// </summary>
+        /// <returns>The front node.</returns>
+        public Node Dequeue()
+        {
+            Node temp = Front;
+            Front = Front.Next;
+            temp.Next = null;
+            return temp;
+        }
+
+        /// <summary>
+        /// Retrieves the node at the front of the Queue.
+        /// </summary>
+        /// <returns>The front node.</returns>
+        public Node Peek()
+        {
+            return Front;
+        }
+    }
+}
---
+++ Data Structures/Stack and Queue/Stack and Queue/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Stack_and_Queue.Classes;
+
+namespace Stack_and_Queue
+{
+    class Program
+    {
+        /// <summary>
+        /// Populates a stack with 9 nodes.
+        /// </summary>
+        /// <param name="stack">The stack being populated.</param>
+        static void MakeStack(Stack stack)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                stack.Push(new Node(i));
+            }
+        }
+
+        /// <summary>
+        /// Populates a queue with 9 nodes.
+        /// </summary>
+        /// <param name="queue">The queue being populated.</param>
+        static void MakeQueue(Queue queue)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                queue.Enqueue(new Node(i));
+            }
+        }
+
+        /// <summary>
+        /// Displays all of the nodes in a stack.
+        /// </summary>
+        /// <param name="stack">The stack being viewed.</param>
+        static void ViewStack(Stack stack)
+        {
+            Node node = stack.Peek();
+            Console.Write("TOP -> ");
+            while (node != null)
+            {
+                Console.Write($"{node.Value} -> ");
+                node = node.Next;
+            }
+            Console.Write("NULL\n");
+        }
+
+        /// <summary>
+        /// Displays all of the nodes in a queue.
+        /// </summary>
+        /// <param name="queue">The queue being viewed.</param>
+        static void ViewQueue(Queue queue)
+        {
+            Node node = queue.Peek();
+            Console.Write("FRONT -> ");
+            while (node.Next != null)
+            {
+                Console.Write($"{node.Value} -> ");
+                node = node.Next;
+            }
+            Console.Write($"{node.Value} <- REAR\n");
+        }
+
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack(new Node(0));
+            Queue queue = new Queue(new Node(0));
+            MakeStack(stack);
+            ViewStack(stack);
+            MakeQueue(queue);
+            ViewQueue(queue);
+            Console.ReadKey();
+        }
+    }
+}
