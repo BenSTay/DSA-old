c6a2bbca2500b04321ca9d0db90ce5e67fd6e67a
Benjamin Taylor <staylor.ben@gmail.com>
2018-07-16 11:28:20 -0700

    Finished hashtable implementation.

---
+++ Data Structures/Hashtables/Hashtables/Classes/Hashtable.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hashtables.Classes
+{
+    public class Hashtable<T>
+    {
+        protected KeyValuePair<string, T>[] Table;
+
+        /// <summary>
+        /// Creates a new instance of the Hashtable class.
+        /// </summary>
+        public Hashtable()
+        {
+            Table = new KeyValuePair<string, T>[1024];
+        }
+
+        /// <summary>
+        /// Adds a new bucket to the hashtable.
+        /// </summary>
+        /// <param name="key">The bucket's key.</param>
+        /// <param name="value">The bucket's value.</param>
+        public void Add(string key, T value)
+        {
+            if (!Contains(key))
+            {
+                int index = GetHash(key);
+                while (index < Table.Length)
+                {
+                    if (Table[index].Key == null)
+                    {
+                        Table[index] = new KeyValuePair<string, T>(key, value);
+                        return;
+                    }
+                    else index++;
+                }
+            }
+            throw new Exception("Key is already in hashtable!");
+        }
+
+        /// <summary>
+        /// Gets the value for a given key in the hashtable.
+        /// </summary>
+        /// <param name="key">The key being searched for.</param>
+        /// <returns>The value associated with the key.</returns>
+        public T Find(string key)
+        {
+            if (Contains(key))
+            {
+                int index = GetHash(key);
+                while (index < Table.Length)
+                {
+                    if (Table[index].Key != key) index++;
+                    else return Table[index].Value;
+                }
+            }
+            throw new Exception("Key is not in hashtable!");
+        }
+
+        /// <summary>
+        /// Checks if a key is present in the hashtable.
+        /// </summary>
+        /// <param name="key">The key being searched for.</param>
+        /// <returns>A boolean representing if the key is present.</returns>
+        public bool Contains(string key)
+        {
+            int index = GetHash(key);
+            while (index < Table.Length)
+            {
+                if (Table[index].Key == key)
+                {
+                    return true;
+                }
+                else index++;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Generates an integer value based on a key's value.
+        /// </summary>
+        /// <param name="key">The key being hashed.</param>
+        /// <returns>A number between 0 and 1023.</returns>
+        public int GetHash(string key)
+        {
+            int hash = 0;
+            foreach(char c in key)
+            {
+                hash += c;
+            }
+            hash *= 599;
+            return hash % 1024;
+        }
+    }
+}
---
+++ Data Structures/Hashtables/Hashtables/Program.cs
@@ -0,0 +1,40 @@
+﻿using Hashtables.Classes;
+using System;
+
+namespace Hashtables
+{
+    public class Program
+    {
+        /// <summary>
+        /// Creates a hashtable with a set of default buckets.
+        /// </summary>
+        /// <returns>A hashtable.</returns>
+        public static Hashtable<int> MakeTable()
+        {
+            Hashtable<int> hashtable = new Hashtable<int>();
+            hashtable.Add("Dog", 0);
+            hashtable.Add("Doe", 17);
+            hashtable.Add("Cat", 1);
+            hashtable.Add("Fish", 2);
+            hashtable.Add("Bird", 3);
+            hashtable.Add("Cow", 4);
+            hashtable.Add("Snake", 5);
+            return hashtable;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] animals = new string[] { "Dog", "Cat", "Doe", "Fish", "Bird", "Cow", "Snake" };
+
+            Hashtable<int> hashtable = MakeTable();
+            foreach(string animal in animals)
+            {
+                if (hashtable.Contains(animal))
+                {
+                    Console.WriteLine($"Key: {animal}, Hash: {hashtable.GetHash(animal)}, Value: {hashtable.Find(animal)}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
